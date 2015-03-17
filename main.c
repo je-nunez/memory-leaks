d8b99da640d0fc16082998ab83a0c72957519bef
yo <yo@localhost.localdomain>
2015-03-17 08:15:17 -0400

    a commit

---
+++ main.c
@@ -0,0 +1,17 @@
+
+#include <stdio.h>
+
+
+
+int
+main(int argc, char ** argv)
+{
+	const char const * memory_allocation_functions[] = { "malloc",
+	                                                     "calloc",
+	                                                     "realloc",
+	                                                     "free" };
+
+
+}
+
+
